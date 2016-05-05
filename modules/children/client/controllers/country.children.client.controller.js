04124b5dfa7a3125aec5a2b9c9225a24f06f48bb
Karl Jorgensen <kjorgens@yahoo.com>
2016-05-04 20:33:06 -0600

    add refresh button for country/stake list

--- modules/children/client/controllers/country.children.client.controller.js
+++ modules/children/client/controllers/country.children.client.controller.js
@@ -9,10 +9,12 @@
 
   function ChildrenCountryController($rootScope, $scope, $state, $translate, ModalService, ChildrenService, ChildrenStakes, PouchService) {
     var vm = this;
+    vm.refreshCountryList = refreshCountryList;
+    vm.onLine = navigator.onLine;
     $translate.use($rootScope.SelectedLanguage);
     function storeDbList(input) {
- //     sessionStorage.setItem('liahonaStakesObject', input);
       vm.liahonaStakes = input;
+      vm.stopSpin();
     }
     function returnFromPut(input) {
       console.log(input);
@@ -20,10 +22,37 @@
     function handleError(input) {
       console.log(input);
     }
+    vm.startSpin = function() {
+      if (!vm.spinneractive) {
+        usSpinnerService.spin('spinner-sync');
+      }
+    };
 
+    vm.stopSpin = function() {
+      if (vm.spinneractive) {
+        usSpinnerService.stop('spinner-sync');
+      }
+    };
+    vm.spinneractive = false;
+
+    $rootScope.$on('us-spinner:spin', function(event, key) {
+      vm.spinneractive = true;
+    });
+
+    $rootScope.$on('us-spinner:stop', function(event, key) {
+      vm.spinneractive = false;
+    });
+    function refreshCountryList(){
+      vm.startSpin();
+      ChildrenStakes.get(function(retVal) {
+        vm.liahonaStakes = retVal;
+        PouchService.createCountryDatabase();
+        PouchService.putStakesLocal(retVal, returnFromPut, handleError);
+      });
+    }
+  
     if (navigator.onLine) {
       ChildrenStakes.get(function(retVal) {
- //       sessionStorage.setItem('liahonaStakesObject', retVal);
         vm.liahonaStakes = retVal;
         PouchService.createCountryDatabase();
         PouchService.putStakesLocal(retVal, returnFromPut, handleError);
