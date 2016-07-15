37c5fa45020bc08aa8700d3b2714109c1d1947c1
Karl Jorgensen <kjorgens@yahoo.com>
2016-07-15 12:27:40 -0600

    add tables for 24+ months

--- modules/children/client/services/children.modals.js
+++ modules/children/client/services/children.modals.js
@@ -1,25 +1,25 @@
-(function () {
-  'use strict';
-
-  angular
-    .module('children.modalService')
-    .factory('ModalService', ModalService);
-
-  ModalService.$inject = ['$q', '$log', '$uibModal'];
-
-  function ModalService($q, $log, $uibModal) {
-    var factory = {};
-
-    factory.progressModal = function (progressString) {
-      return $uibModal.open({
-        templateUrl: 'modules/children/client/templates/pouchSync.html',
-        controller: 'ChildrenModalController',
-        resolve: {
-          progressString: progressString
-        }
-      }).result;
-    };
-    return factory;
-  }
-}());
+// (function () {
+//   'use strict';
+//
+//   angular
+//     .module('children.modalService')
+//     .factory('ModalService', ModalService);
+//
+//   ModalService.$inject = ['$q', '$log', '$uibModal'];
+//
+//   function ModalService($q, $log, $uibModal) {
+//     var factory = {};
+//
+//     factory.progressModal = function (progressString) {
+//       return $uibModal.open({
+//         templateUrl: 'modules/children/client/templates/pouchSync.html',
+//         controller: 'ChildrenModalController',
+//         resolve: {
+//           progressString: progressString
+//         }
+//       }).result;
+//     };
+//     return factory;
+//   }
+// }());
 
