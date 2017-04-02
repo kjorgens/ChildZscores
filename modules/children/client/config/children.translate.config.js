7a4e694a1015fb274d0c355606b7faf60f918628
Karl Jorgensen <kjorgens@yahoo.com>
2017-04-02 10:24:33 -0600

    finalize canary bug fixes

--- modules/children/client/config/children.translate.config.js
+++ modules/children/client/config/children.translate.config.js
@@ -123,7 +123,7 @@
       INVALID_DELIVERY_DATE: 'Invalid delivery date',
       CORRECT_DELIVERY_DATE: 'Enter correct delivery date',
       INVALID_CHILDS_BDATE: 'Invalid childs age',
-      CORRECT_CHILD_BDATE: 'Must be 0 to 3 years',
+      CORRECT_CHILD_BDATE: 'Must be 0 to 6 months',
       NEW_FEATURES: ' New search and filter features: You can now search by typing the name in the search window.' +
       'If you type the name of an existing child, you will be taken to the screen where you enter a new screening.' +
       'Search is also performed after you enter first and last names in the new child form.' +
@@ -246,7 +246,7 @@
       INVALID_DELIVERY_DATE: 'Fecha de entrega no válida',
       CORRECT_DELIVERY_DATE: 'Introduzca la fecha de entrega correcta',
       INVALID_CHILDS_BDATE: 'Edad de los niños no válidos',
-      CORRECT_CHILD_BDATE: 'Debe ser de 0 a 3 años',
+      CORRECT_CHILD_BDATE: 'Debe ser de 0 a 6 meses',
       NEW_FEATURES: ' Nuevas funciones de búsqueda y filtro: Ahora puede buscar escribiendo el nombre en la ventana de búsqueda.' +
       'Si escribe el nombre de un niño existente, se le llevará a la pantalla para ingresar a una nueva proyección.' +
       'La búsqueda también se realiza, después de introducir los nombres y apellidos en el formulario nuevo hijo.' +
