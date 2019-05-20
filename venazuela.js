101fa413adda74ce70f3e83e386e477fb86da0d6
karljorgensen <kjorgens>
2019-05-19 18:02:44 -0600

    add venezuela

---
+++ venazuela.js
@@ -0,0 +1,17 @@
+const venezuelaList = {
+  'countries': [
+    {
+      'name': 'Venezuela',
+      'code': 'VEN',
+      'image': 'modules/children/client/img/venezuela.png',
+      'stakes': [
+        {
+          'stakeName': 'Test Venezuela',
+          'stakeDB': 'test_stake_ven'
+        }
+      ]
+    }
+  ]
+};
+
+module.exports = venezuelaList;
