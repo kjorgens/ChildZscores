b7a6ef7713629268d03797e26863360ee07fb040
karljorgensen <kjorgens>
2019-07-31 08:46:58 -0600

    fix a stakename spelling issue

--- modules/children/server/controllers/children.server.controller.js
+++ modules/children/server/controllers/children.server.controller.js
@@ -641,7 +641,7 @@ async function getChildAndData(parmObj, multiplier) {
       // console.log(`${parmObj.stakeName} just emitted from get childAndData on server`);
       var newObj = Object.assign({}, parmObj);
       var stake = parmObj.stakeDB;
-      return resolve(Promise.join(newDBRequest(stake, parmObj.stakename, newObj, 'children_list'), newDBRequest(stake, parmObj.stakename, newObj, 'scr_list')));
+      return resolve(Promise.join(newDBRequest(stake, parmObj.stakeName, newObj, 'children_list'), newDBRequest(stake, parmObj.stakeName, newObj, 'scr_list')));
     }
     // console.log(`start ${ parmObj.stakeName } in ${ multiplier } seconds`);
     return setTimeout(accessDB, 750 * multiplier, parmObj);
