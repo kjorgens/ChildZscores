4c408b67f2525fe6082f46b7c0d2d13f34cede39
kjorgens <kjorgens@yahoo.com>
2020-04-26 08:56:37 -0600

    fix: testing env var

--- ghActionScripts/analyze-comments/analyze-comments.js
+++ ghActionScripts/analyze-comments/analyze-comments.js
@@ -4,8 +4,8 @@ const github = require('@actions/github');
 (async () => {
   try {
     // `who-to-greet` input defined in action metadata file
-    const myToken = core.getInput('repo-token');
-    console.log(`token = ${ myToken }`);
+    // const myToken = core.getInput('repo-token');
+    // console.log(`token = ${ myToken }`);
     // const octokit = new github.GitHub(myToken);
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
