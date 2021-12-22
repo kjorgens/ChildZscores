857f4ff988e11b0d3d621d68e67aa3331e291d2c
kjorgens <kjorgens@yahoo.com>
2021-12-22 12:42:08 -0700

    fix: webpack testing

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js");
 
 if (workbox) {
 
